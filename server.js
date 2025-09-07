494cab6c1a99b4c852353d3c9c461ac535586532
Ahmed Omran <ahmedomran1950@gmail.com>
2025-09-07 21:43:03 +0300

    jjj

--- server.js
+++ server.js
@@ -34,7 +34,11 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://todo-list.2ao1.space",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
